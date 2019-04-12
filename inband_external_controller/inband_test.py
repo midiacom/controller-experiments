120df1b5b7a14a6882efbd9de13066839eb8e9dd
Juan Lucas Vieira <juanlucasvieira@id.uff.br>
2019-04-12 12:22:19 -0300

    Add directory for tests

--- inband_external_controller/inband_test.py
+++ inband_external_controller/inband_test.py
@@ -12,6 +12,10 @@ def ovsns(user, ctrl_ip='192.168.1.13'):
 
     "Create an empty network and add nodes to it."
 
+    bandw = 10
+    rate = 10
+    pckt_num = 20
+
     mn = Mininet(topo=None, build=False, link=TCLink)
 
     h1 = mn.addHost('h1', ip='192.168.1.3', mac='00:00:00:00:00:01')
@@ -20,7 +24,7 @@ def ovsns(user, ctrl_ip='192.168.1.13'):
     h2 = mn.addHost('h2', ip='192.168.1.4', mac='00:00:00:00:00:02')
     
     # c0 = mn.addHost( 'c0', ip=ctrl_ip, mac='00:00:00:00:00:13' ) # This is a host!
-    bandwith = dict(bw=10)
+    bandwith = dict(bw=bandw)
     mn.addLink(h1, s1, **bandwith)
     mn.addLink(s1, s2, **bandwith)
     mn.addLink(h2, s2, **bandwith)
@@ -46,27 +50,35 @@ def ovsns(user, ctrl_ip='192.168.1.13'):
         h.cmd('sysctl -w net.ipv6.conf.default.disable_ipv6=1')
         h.cmd('sysctl -w net.ipv6.conf.lo.disable_ipv6=1')
 
-    s1.cmd('tcpdump -i s1-eth0 -w logs/s1-eth0.pcap port 6653 or ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
-    s1.cmd('tcpdump -i s1-eth1 -w logs/s1-eth1.pcap port 6653 or ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
-    s2.cmd('tcpdump -i s2-eth0 -w logs/s2-eth0.pcap port 6653 or ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
-    s2.cmd('tcpdump -i s2-eth1 -w logs/s2-eth1.pcap port 6653 or ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
-    s2.cmd('tcpdump -i enp1s0 -w logs/enp1s0.pcap port 6653 or ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
-    h1.cmd('tcpdump -i h1-eth0 -w logs/h1.pcap ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
-    h2.cmd('tcpdump -i h2-eth0 -w logs/h2.pcap ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
+    logdir = ("logs_bw" + str(bandw)+"_tx"+str(rate)+"_"+str(pckt_num)+"k")
 
-    h1.cmd('date > logs/inicio.txt ')
-    h1.cmd("top -b -d 1 | grep 'mn\|ovs\|tcpdump' >> logs/top.txt &")
+    s1.cmd('tcpdump -i s1-eth0 -w '+str(logdir)+'/s1-eth0.pcap port 6653 or ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
+    s1.cmd('tcpdump -i s1-eth1 -w '+str(logdir)+'/s1-eth1.pcap port 6653 or ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
+    s2.cmd('tcpdump -i s2-eth0 -w '+str(logdir)+'/s2-eth0.pcap port 6653 or ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
+    s2.cmd('tcpdump -i s2-eth1 -w '+str(logdir)+'/s2-eth1.pcap port 6653 or ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
+    s2.cmd('tcpdump -i enp1s0 -w '+str(logdir)+'/enp1s0.pcap port 6653 or ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
+    h1.cmd('tcpdump -i h1-eth0 -w '+str(logdir)+'/h1.pcap ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
+    h2.cmd('tcpdump -i h2-eth0 -w '+str(logdir)+'/h2.pcap ether src 00:00:00:00:00:01 or ether dst 00:00:00:00:00:02 or ether dst 00:00:00:00:00:01 &')
+
+    h1.cmd('mkdir '+str(logdir)+'')
+
+    h1.cmd('date > '+str(logdir)+'/inicio.txt ')
+    h1.cmd("top -b -d 1 | grep 'mn\|ovs\|tcpdump' >> "+str(logdir)+"/top.txt &")
 
     h2.cmd('ping -c 1 192.168.1.3')
 
     sleep(20)
 
-    h1.cmd('tcpreplay -i h1-eth0 -K --mbps 10 packets_20k.pcap > logs/tcpreplay_info.txt')
+    h1.cmd('tcpreplay -i h1-eth0 -K --mbps '+str(rate)+' packets_'+str(pckt_num)+'k.pcap > '+str(logdir)+'/tcpreplay_info.txt')
 
     # s1.cmd('ovs-ofctl dump-flows s1 > logs/flows_s1.log 2>&1 &')
     # s2.cmd('ovs-ofctl dump-flows s1 > logs/flows_s1.log 2>&1 &')
 
     CLI(mn)
+
+    sleep(30)
+
+    h1.cmd('chmod 777 -R '+str(logdir)+'')
 
     s1.cmd('killall -2 tcpdump')
 
